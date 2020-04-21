008b3734e63f60993c427802ac42071493b5e24e
Robert Isaac <robert.isaac@orange.com>
2020-04-21 13:45:44 +0200

    test-library

---
+++ projects/test-library/src/lib/test-library.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-test-library',
+  template: `
+    <p>
+      test-library works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class TestLibraryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
